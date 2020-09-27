ae956ed66359fa6b48f46808ac737c78e4356ca0
Artur <nikitsin.artur@gmail.com>
2020-09-27 23:12:45 +0300

    fix: delete footer braskets

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./footer.scss";
 import { FOOTER_TITLE } from "../constants/titles";
 
-function Footer({}) {
+function Footer() {
   return <div className='footer'>
     <p className='allRights'>{FOOTER_TITLE}</p>
   </div>;
