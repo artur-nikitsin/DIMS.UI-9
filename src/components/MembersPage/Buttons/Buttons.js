92a9f8e0e9229b967a88ca0e3a1b228d4d6e9abc
Artur <nikitsin.artur@gmail.com>
2020-10-03 23:14:36 +0300

    feature: add reactstrap buttons to MembersPage

--- src/components/MembersPage/Buttons/Buttons.js
+++ src/components/MembersPage/Buttons/Buttons.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import './buttons.scss';
+import React from "react";
+import "./buttons.scss";
+import { Button } from "reactstrap";
 
 function Buttons(props) {
   return (
     <div className='membersButtonsGroup'>
-      <button className='progressMemberButton' id={props.userId} onClick={props.handleProgress}>
+
+      <Button outline color="secondary" className='progressMemberButton' id={props.userId}
+              onClick={props.handleProgress}>
         Progress
-      </button>
-      <button className='tasksMemberButton' id={props.userId} onClick={props.handleTasks}>
+      </Button>
+      <Button outline color="secondary" className='tasksMemberButton' id={props.userId}
+              onClick={props.handleTasks}>
         Tasks
-      </button>
-      <button className='editMemberButton' id={props.userId} onClick={props.handleEdit}>
+      </Button>
+      <Button outline color="secondary" className='editMemberButton' id={props.userId}
+              onClick={props.handleEdit}>
         Edit
-      </button>
-      <button className='deleteMemberButton' id={props.userId} onClick={props.handleDelete}>
+      </Button>
+      <Button outline color="danger" className='deleteMemberButton' id={props.userId}
+              onClick={props.handleDelete}>
         Delete
-      </button>
+      </Button>
     </div>
   );
 }
