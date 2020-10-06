18ebcab383892aba9eb0be5b852c39811b1f7fe7
Artur <nikitsin.artur@gmail.com>
2020-10-06 11:20:23 +0300

    feature: add dev incubator logo for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./header.scss";
-import { Button } from 'reactstrap';
+import { Button } from "reactstrap";
 
 function Header({ isLogin, handleLogout }) {
   return (
     <div className='header'>
+      <div className="logoContainer"><img className="devIncubatorLogo" src="assets/img/logo.png" alt="devIncubator" />
+      </div>
       {isLogin && <Button outline color="secondary" className='logoutButton' onClick={handleLogout}>Logout</Button>}
     </div>
   );
